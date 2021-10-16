e12414daf3ce3901ef7ff211e95438bc54fe996a
Yaowei Zong <luke0zong@gmail.com>
2021-10-16 12:35:24 -0400

    Initial Commit

---
+++ instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagram
+//
+//  Created by Yaowei on 10/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
